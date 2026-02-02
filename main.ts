ea2949ac94630176166d5d38f8afb4d92aa89dfe
HassanMoeZzz <hassan.mohamed@evolvextechnology.com>
2026-02-02 17:10:27 +0400

    using map abstraction

--- main.ts
+++ main.ts
@@ -11,22 +11,15 @@ const bob: IUser = { name: "Bob", level: 10, status: "Online" };
 const tom: IUser = { name: "Tom", level: 20, status: "In Action" };
 
 // Generic HashMap type
-type HashMap<T> = Record<string, T>;
+type HashMap<T> = Record<IUser["name"], T>;
 
 // Create users hashmap
-const users: HashMap<IUser> = {
+const usersObject: HashMap<IUser> = {
   [bob.name]: bob,
   [tom.name]: tom,
 };
 
-// Add a new user
-// function addUser(user: IUser) {
-//   users[user.name] = user;
-// }
-
-// Accessing a user safely
-// function getUser(name: string): IUser | undefined {
-//   return users[name];
-// }
-
-console.log("All users:", users);
+// Create fancy users hashmap
+const usersHash = new Map<IUser["name"], IUser>();
+usersHash.set(bob.name, bob);
+usersHash.set(tom.name, tom);
